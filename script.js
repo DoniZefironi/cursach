0047b7e842d7fc3995f750785ad3b9c6f5eaf075
Vladislav Shabolda <VladNeckt.gmail.com>
2024-05-14 17:18:09 +0300

    feat: add new function and animation

--- script.js
+++ script.js
@@ -68,6 +68,34 @@ fetch('slides.json')
 
 
 
+  var burger = document.querySelector('.burger-menu');
+  var navigat = document.querySelector('.navigat');
+  
+  burger.addEventListener('click', function() {
+    navigat.classList.toggle('open'); 
+  });
+
+
+
+  var themeButton = document.getElementById("themeButton");
+  var body = document.body;
+  var honest = document.getElementsByClassName("honest");
+  var footer = document.getElementsByClassName("footer");
+  
+  themeButton.addEventListener("click", function() {
+    body.classList.toggle("dark");
+    honest.classList.toggle("dark");
+    footer.classList.toggle("dark");
+  
+    if (body.classList.contains("dark")) {
+      themeButton.textContent = "Светлая тема";
+    } else {
+      themeButton.textContent = "Темная тема";
+    }
+  });
+
+
+
   var translations = {};
 
   function loadTranslations(callback) {
