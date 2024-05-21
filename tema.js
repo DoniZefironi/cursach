0c776c2a870f57c0c8310311a37c7d0edbf6a493
Vladislav Shabolda <VladNeckt.gmail.com>
2024-05-21 14:24:35 +0300

    feat: add new file and local storage

--- tema.js
+++ tema.js
@@ -1,16 +1,33 @@
 var themeButton = document.getElementById("themeButton");
 var body = document.body;
-var honest = document.getElementsByClassName("honest");
-var footer = document.getElementsByClassName("footer");
+var honest = document.getElementsByClassName("honest")[0];
+var footer = document.getElementsByClassName("footer")[0];
+
+var savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+  body.classList.add(savedTheme);
+  honest.classList.add(savedTheme);
+  footer.classList.add(savedTheme);
+
+  if (savedTheme === "dark") {
+    themeButton.textContent = "Светлая тема";
+  } else {
+    themeButton.textContent = "Темная тема";
+  }
+}
 
 themeButton.addEventListener("click", function() {
+  localStorage.setItem("theme", "dark");
   body.classList.toggle("dark");
   honest.classList.toggle("dark");
   footer.classList.toggle("dark");
 
   if (body.classList.contains("dark")) {
+
     themeButton.textContent = "Светлая тема";
   } else {
+    localStorage.setItem("theme", "light");
     themeButton.textContent = "Темная тема";
   }
 });
